9767a57a148ad83dfea7aeca38436542c29b49a4
stoor812 <shaantoor812@gmail.com>
2026-07-30 01:22:42 -0400

    Add: permutation-string - submission-7

---
+++ Data Structures & Algorithms/permutation-string/submission-7.py
@@ -0,0 +1,53 @@
+class Solution:
+    def checkInclusion(self, s1: str, s2: str) -> bool:
+        left = 0
+        right = len(s1) - 1
+        hashOne = {}
+        hashTwo = {}
+
+        #BASE CASE
+        if len(s1) > len(s2): return False
+
+        # HASH S1
+        for i in s1:
+            if i in hashOne:
+                hashOne[i] += 1
+            else:
+                hashOne[i] = 1
+        
+        # HASH S2 (FIXED LEN(S1))
+        for i in range(len(s1)):
+            key = s2[i]
+            if key in hashTwo:
+                hashTwo[key] += 1
+            else:
+                hashTwo[key] = 1
+
+        # CHECK INITIAL WINDOW
+        if hashOne == hashTwo:
+            return True
+
+        # SLIDING WINDOW
+        while right < len(s2) - 1:
+            #REMOVE LEFT
+            hashTwo[s2[left]] -= 1
+            if hashTwo[s2[left]] == 0:
+                del hashTwo[s2[left]]
+
+            # SLIDE WINDOW
+            left += 1
+            right += 1
+                
+            # ADD RIGHT TO HASH
+            if s2[right] in hashTwo:
+                hashTwo[s2[right]] += 1
+            else:
+                hashTwo[s2[right]] = 1
+                
+            # CHECK WINDOW
+            if hashOne == hashTwo:
+                return True
+
+        return False
+
+            
